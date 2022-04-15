a94e873652085e5dedc0d68fd8318b6af44681c2
İrfan <irfanbykara@gmail.com>
2022-04-15 11:11:23 +0300

    'Initial'

--- django_cameralyze_api/urls.py
+++ django_cameralyze_api/urls.py
@@ -19,7 +19,8 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('base.api.urls'))
+    path('api/', include('base.api.urls')),
+    path( '', include( 'base.urls' ) )
 
 ]
 
